b7ee6fc820abc14f0b43a655ff68d31bd1e3365b
Andrei Nicolae <andrei.nicolae@vodafone.com>
2020-06-02 06:51:35 +0100

    Fix dynamo methods

--- libs/dynamodb-lib.js
+++ libs/dynamodb-lib.js
@@ -5,7 +5,7 @@ const client = new AWS.DynamoDB.DocumentClient();
 export default {
   get: params => client.get(params).promise(),
   put: params => client.put(params).promise(),
-  query: params => client.put(params).promise(),
-  update: params => client.put(params).promise(),
-  delete: params => client.put(params).promise(),
+  query: params => client.query(params).promise(),
+  update: params => client.update(params).promise(),
+  delete: params => client.delete(params).promise(),
 };
